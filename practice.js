222650ecae4449ab773a97903ff793a7c82d946c
MRV-dev <a.marv0903@gmail.com>
2026-06-04 19:00:50 +0800

    59

--- practice.js
+++ practice.js
@@ -6,7 +6,42 @@ function add()
   let c;
   c = a + b;
   return c;
+  // console.log(c);
 }
 
+// add();
 let res = add();
 console.log(res);
+
+
+
+
+
+
+
+function calcGrade(studentMarks, maxMarks){
+  let percentage = (studentMarks / maxMarks) * 100;
+  let grade;
+
+  if(percentage >= 90)
+  {
+    studentGrade = 'A';
+  }
+  else if(percentage >= 80)
+  {
+    studentGrade = 'B';
+  }
+  else if(percentage >= 70)
+  {
+    studentGrade = 'C';
+  }
+  else
+  {
+    studentGrade = 'F';
+  }
+
+  console.log(studentGrade);
+}
+
+calcGrade(68, 100);
+calcGrade(86, 100);
